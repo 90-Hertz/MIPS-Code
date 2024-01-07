edb03c37d84be96e81da859d65ce92f3abd6acb8
Ngô Việt Hoàng <91136269+90-Hertz@users.noreply.github.com>
2024-01-07 18:01:53 +0700

    Add files via upload

---
+++ mips2.asm
@@ -0,0 +1,36 @@
+
+.data
+str: .asciiz""
+str1: .asciiz"Nhap mot ki tu "
+str2: .asciiz"\nKi tu lien truoc " 
+str3: .asciiz"\nKi tu lien sau "
+
+.text
+li $v0, 4
+la $a0, str1
+syscall
+
+li $v0, 12
+syscall
+move $t0, $v0
+
+
+
+add $t1, $t0, 1
+sub $t2, $t0, 1
+
+li $v0, 4
+la $a0, str2
+syscall
+
+li $v0, 11
+la $a0, ($t2)
+syscall
+
+li $v0, 4
+la $a0, str3
+syscall
+
+li $v0, 11
+la $a0, ($t1)
+syscall
