10fa685773df150ae3381fee7aa928312c0ee88b
Ngô Việt Hoàng <91136269+90-Hertz@users.noreply.github.com>
2024-01-07 17:44:17 +0700

    Add files via upload

---
+++ mips1.asm
@@ -0,0 +1,23 @@
+
+.data
+str: .asciiz""
+str1: .asciiz"Nhap mot chuoi "
+str2: .asciiz"Chuoi da nhap " 
+
+.text
+li $v0, 4
+la $a0, str1
+syscall
+
+li $v0, 8
+la $a0, str
+li $a1, 100
+syscall
+
+li $v0, 4
+la $a0, str2
+syscall
+
+li $v0, 4
+la $a0, str
+syscall
